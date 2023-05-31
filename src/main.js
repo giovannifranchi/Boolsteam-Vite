d42e7a18089fad12e7527091d7a64059dc25b366
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-31 20:07:57 +0200

    Add: icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import './assets/index-js/index';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faDownload, faMagnifyingGlass, faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faDownload, faMagnifyingGlass, faBookmark,faFacebook,faTwitter);
