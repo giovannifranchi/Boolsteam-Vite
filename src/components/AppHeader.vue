231bac3e08ac8d72ab24ab5d44845e2c48ef50c5
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-31 12:25:34 +0200

    Add: app header nav links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,27 @@
 <template>
   <!-- header -->
-  <header class="py-5">
+  <header class="py-4">
     <!-- container -->
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <img src="../assets/img/steam-logo.png" alt="steam-logo" class="w-25" />
-      </a>
+    <div class="container d-flex align-items-center">
+        <!-- Logo -->
+        <a href="#" class="pe-5">
+            <img src="../assets/img/steam-logo.png" alt="steam-logo" class="ms-logo" />
+        </a>
+      <!-- list -->
+      <ul class="list-unstyled m-0  d-flex gap-3 px-5">
+        <li>
+            <a href="#" class="text-decoration-none text-reset">NEGOZIO</a>
+        </li>
+        <li>
+            <a href="#" class="text-decoration-none text-reset">COMMUNITY</a>
+        </li>
+        <li>
+            <a href="#" class="text-decoration-none text-reset">INFORMAZIONI</a>
+        </li>
+        <li>
+            <a href="#" class="text-decoration-none text-reset">ASSISTENZA</a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -22,5 +38,8 @@ export default {
 header {
   background-color: $primary;
   color: $text-primary;
+  .ms-logo {
+    width: 12.5rem;
+  }
 }
 </style>
