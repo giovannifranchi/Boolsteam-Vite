3c98dd8fe0e936eefe8d388f71667be3f316ab8f
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-31 19:30:23 +0200

    Fix: paddings

--- src/components/HomePage/HomeGamesList.vue
+++ src/components/HomePage/HomeGamesList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ms-container container">
+    <div class="ms-container">
         <h3 class="text-white">I Giochi</h3>
         <ul class="list-unstyled m-0 p-0">
             <li v-for="(game, index) in getAllGames" :key="index" class="mb-3 ms-card">
@@ -38,6 +38,7 @@ import CardList from '../utils/CardList.vue';
 @use '../../assets/partials/variables' as *;
 .ms-container {
     background-color: $quternary;
+    padding: 30px;
     .ms-card {
         background-color: $tertiary;
         box-shadow: 0px 13px 39px 13px rgba(9, 9, 9, 0.8);
--- src/components/utils/CardList.vue
+++ src/components/utils/CardList.vue
@@ -3,15 +3,15 @@
         <div class="img-container w-25">
             <img :src="info.thumb" :alt="info.game">
         </div>
-        <div class="details w-50">
+        <div class="details w-50 ps-5">
             <h3>{{ info.game }}</h3>
             <ul class="list-unstyled d-flex gap-3 m-0 p-0">
                 <li class="flex-grow-1" v-for="(genre, index) in info.genres" :key="index">
                     <span class="badge text-bg-dark w-100">{{ genre.name }}</span>
                 </li>
             </ul>
-            <div class="d-flex">
-                <span>{{ info.release_date }}</span>
+            <div class="d-flex gap-4 py-4">
+                <strong>{{ info.release_date }}</strong>
                 <span>Icons</span>
             </div>
         </div>
