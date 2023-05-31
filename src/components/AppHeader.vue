6d18b9179c9d4fdf8a3ffcf00ac73c22ef224cc8
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-31 13:09:15 +0200

    Add: header completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,34 +1,66 @@
 <template>
   <!-- header -->
-  <header class="py-4">
+  <header class="">
     <!-- container -->
-    <div class="container d-flex align-items-center">
-        <!-- Logo -->
-        <a href="#" class="pe-5">
-            <img src="../assets/img/steam-logo.png" alt="steam-logo" class="ms-logo" />
-        </a>
+    <div class="container d-flex align-items-center justify-content-between">
+      <!-- Logo -->
+      <a href="#" class="py-3">
+        <img src="../assets/img/steam-logo.png" alt="steam-logo" class="ms-logo" />
+      </a>
       <!-- list -->
-      <ul class="list-unstyled m-0  d-flex gap-3 px-5">
+      <!-- TODO: need to be changed with routerlink -->
+      <ul class="list-unstyled m-0 d-flex gap-3 py-3">
         <li>
-            <a href="#" class="text-decoration-none text-reset">NEGOZIO</a>
+          <a href="#" class="text-decoration-none text-reset">NEGOZIO</a>
         </li>
         <li>
-            <a href="#" class="text-decoration-none text-reset">COMMUNITY</a>
+          <a href="#" class="text-decoration-none text-reset">COMMUNITY</a>
         </li>
         <li>
-            <a href="#" class="text-decoration-none text-reset">INFORMAZIONI</a>
+          <a href="#" class="text-decoration-none text-reset">INFORMAZIONI</a>
         </li>
         <li>
-            <a href="#" class="text-decoration-none text-reset">ASSISTENZA</a>
+          <a href="#" class="text-decoration-none text-reset">ASSISTENZA</a>
         </li>
       </ul>
+      <!-- options -->
+      <div class="options align-self-start d-flex align-items-start">
+        <div class="install p-1">
+          <font-awesome-icon icon="fa-regular fa-download" />
+          <span>Installa Steam</span>
+        </div>
+        <div class="actions d-flex ps-2 py-1">
+          <a href="" class="text-reset px-2 ms-border text-decoration-none">ACCEDI</a>
+          <!-- dropdown -->
+          <div class="dropdown">
+            <a
+              class=" dropdown-toggle text-reset px-2 text-decoration-none"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              LINGUA
+            </a>
+
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
   name: "AppHeader",
+  components: { FontAwesomeIcon },
 };
 </script>
 
@@ -40,6 +72,13 @@ header {
   color: $text-primary;
   .ms-logo {
     width: 12.5rem;
+  }
+  .install {
+    background-color: $tertiary-green;
+    color: #fff;
+  }
+  .ms-border {
+    border-right: 2px solid $text-primary;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import store from './store/store';
-import App from './App.vue'
-import './assets/index-js/index'
+import App from './App.vue';
+import './assets/index-js/index';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faDownload);
 
 createApp(App)
 .use(store)
+.component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
